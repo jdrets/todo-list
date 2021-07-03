bbe92ca277ff5eddb46601626123004417c73d0a
Julian Drets <jdrets@santandertecnologia.com.ar>
2021-07-03 01:59:17 -0300

    order css

--- src/components/Button/index.spec.tsx
+++ src/components/Button/index.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
-import { render, fireEvent, waitFor } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
 import Button from '.'
 import theme from '../../utils/constants/theme'
@@ -27,7 +27,7 @@ describe('<Button />', () => {
     expect(container).toMatchSnapshot()
   })
 
-  test('should call onClick when button is clicked',() => {
+  test('should call onClick when button is clicked', () => {
     const onClickMock = jest.fn()
     const { getByText } = setup({
       onClick: onClickMock
